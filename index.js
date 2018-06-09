f53fdbd882e8eaa7249d79df8e44fcb491acf0a6
joe-re <joe.tialtngo@gmail.com>
2018-06-10 04:12:33 +0900

    restore

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.parse         = require('./lib/parser').parse;
+exports.Parser        = require('./lib/parser');
 exports.AstHelper     = require('./lib/ast_helper');
 exports.AstReader     = require('./lib/ast_helper').Reader;
