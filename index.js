bf765240235e77b9f208667942ed2191a46d0bc5
joe-re <joe.tialtngo@gmail.com>
2020-05-17 21:52:59 +0800

    add default export

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ exports.parseFromClause = require('./lib/parser').parseFromClause;
 exports.Parser          = require('./lib/parser');
 exports.AstHelper       = require('./lib/ast_helper');
 exports.AstReader       = require('./lib/ast_helper').Reader;
+module.exports = {
+  parse: exports.parse,
+  parseFromClause: exports.parseFromClause,
+  Parser: exports.Parser,
+  AstHelper: exports.AstHelper,
+  AstReader: exports.AstReader
+}
